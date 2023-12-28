a3426639d62dca578063970a406bb49fd0836727
Anant-Singh-Chauhan <eng.anant.schauhan21@gmail.com>
2023-12-28 21:13:54 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { PortfolioColors } from './commons/portfolioColors';
+
+function App() {
+  return (
+    <div className="App" style={{backgroundColor: PortfolioColors.algae}}>
+
+      <header className="App-header">
+     
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/commons/portfolioColors.js
@@ -0,0 +1,12 @@
+
+///
+/// static [PortfolioColors] class to represent colors 
+///
+export class PortfolioColors{
+    static greySoil = '#B7B7A4';
+    static blush = '#CB997E';
+    static concealer = '#FFE8D6';
+    static peach = '#DDBEA9';
+    static wetCement = '#A5A58D';
+    static algae = '#6B705C';
+} 

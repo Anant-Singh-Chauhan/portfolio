45eec09d97d5502a2806000230cea8c79420c96b
Anant-Singh-Chauhan <eng.anant.schauhan21@gmail.com>
2024-04-06 19:48:51 +0530

    updated action logos and logic

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,6 +1,8 @@
 import { React, useContext } from "react";
 import { ProjectContext } from "../../store/projects-context";
 import { SelfProjects as PROJECTS } from "../../commons/masterData/masterData";
+import { ReactComponent as GithubLogo } from "../../assests/images/tech_icons/githubLogo.svg";
+import { ReactComponent as LiveLogo } from "../../assests/images/tech_icons/liveLogo.svg";
 
 export default function ProjectDetails() {
   const { selectedProjectId } = useContext(ProjectContext);
@@ -9,7 +11,30 @@ export default function ProjectDetails() {
       {PROJECTS.filter((element) => element.id == selectedProjectId).map(
         (prj) => (
           <div key={prj.id}>
-            <h2 className="project_title">{prj.title}</h2>
+            <div className="d-flex-row project_title">
+              <h2 className="">{prj.title}</h2>
+              <div className="d-flex-row project_action_div">
+                {prj.liveLink.length != 0 && (
+                  <a
+                    className="project_action_item"
+                    href={prj.liveLink}
+                    target="blank"
+                  >
+                    <LiveLogo/>
+                  </a>
+                )}
+                {prj.githubLink.length != 0 && (
+                  <a
+                    className="project_action_item"
+                    href={prj.githubLink}
+                    target="blank"
+                  >
+                    <GithubLogo/>
+
+                  </a>
+                )}
+              </div>
+            </div>
             <ul className="project_description">
               {prj.description.map((desc) => (
                 <li key={prj.id + Math.random() * 1000}>{desc}</li>
@@ -19,29 +44,8 @@ export default function ProjectDetails() {
             <div className="toolsUsed">
               Tools Used:{" "}
               {prj.toolsUsed.map((tool) => (
-                <span key={prj.id+tool}>{tool}, </span>
+                <span key={prj.id + tool}>{tool}, </span>
               ))}
-            </div>
-
-            <div className="d-flex-row project_action_div">
-              {prj.liveLink.length != 0 && (
-                <a
-                  className="project_action_item"
-                  href={prj.liveLink}
-                  target="blank"
-                >
-                  Visit Project
-                </a>
-              )}
-              {prj.githubLink.length != 0 && (
-                <a
-                  className="project_action_item"
-                  href={prj.githubLink}
-                  target="blank"
-                >
-                  View Source Code
-                </a>
-              )}
             </div>
           </div>
         )

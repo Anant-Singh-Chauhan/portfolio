6753e96c4c4457eb0a98b66bcc0ee1d115eb55e7
Anant Singh Chauhan <anant.c@gormalone.com>
2024-04-10 13:29:40 +0530

    bugFix: reversing project menu

--- src/components/projects/ProjectsMenu.js
+++ src/components/projects/ProjectsMenu.js
@@ -2,14 +2,15 @@ import {React, useContext} from 'react';
 import { SelfProjects as PROJECTS }  from '../../commons/masterData/masterData';
 import './projects.css';
 import { ProjectContext } from '../../store/projects-context';
+const projects = PROJECTS.reverse();
+
 
 export default function ProjectsMenu() {
-
     const {selectedProjectId,handleProjectSelection} = useContext(ProjectContext)
   return (
     <div className="project_menu ">
        {
-        PROJECTS.reverse().map(element => 
+        projects.map(element => 
             <div key={"project"+element.id} className= {element.id == selectedProjectId ? "project_menu_item active":"project_menu_item"} onClick={()=>handleProjectSelection(element.id)}>
                 {element.title} 
             </div> )

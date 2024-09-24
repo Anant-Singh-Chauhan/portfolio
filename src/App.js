cdf2da42629b5e405c14960d5303712635e9bcd3
Anant Singh Chauhan <anant.c@gormalone.com>
2024-09-25 00:06:10 +0530

    added logClient functionality

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { React, useEffect } from 'react';
+import { logClientInfo } from './loggerService';
 import './App.css';
 import Header from './components/header/Header';
 import ProfileSummary from './components/profileSummary/ProfileSummary';
@@ -6,7 +8,11 @@ import './commons/common.css';
 import Projects from './components/projects/Projects';
 import Footer from './components/footer/Footer';
 
+
 function App() {
+  useEffect(() => {
+    logClientInfo('info', 'Portfolio accessed.');
+  }, []);
   return (
     <div className="App border-1-1-solid" >
       <Header/>

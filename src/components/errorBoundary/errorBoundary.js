42e9fe6172d7a204416232a432f12d2c52ea0317
Anant Singh Chauhan <anant.c@gormalone.com>
2024-09-25 01:02:40 +0530

    page reload in case of error

--- src/components/errorBoundary/errorBoundary.js
+++ src/components/errorBoundary/errorBoundary.js
@@ -15,12 +15,21 @@ class ErrorBoundary extends React.Component {
   componentDidCatch(error, info) {
     // You can log the error to an error reporting service
     logClientInfo("error", error.toString(), info.componentStack);
+
+    // Refresh the page after 10 seconds
+    setTimeout(() => {
+        window.location.reload();
+      }, 10000);
   }
 
   render() {
     if (this.state.hasError) {
       // You can render any custom fallback UI
-      return <h1>Something went wrong.</h1>;
+      return (
+        <div>
+          <h1>Something went wrong. The page will refresh in 10 seconds.</h1>
+        </div>
+      );
     }
 
     return this.props.children;

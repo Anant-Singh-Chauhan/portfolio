bde258243629386a55acf5925175a0f22c659243
Anant Singh Chauhan <anant.c@gormalone.com>
2024-09-25 19:21:41 +0530

    update for user IP

--- src/loggerService.js
+++ src/loggerService.js
@@ -1,15 +1,23 @@
-import axios from 'axios';
-import UAParser from 'ua-parser-js';
+import axios from "axios";
+import UAParser from "ua-parser-js";
 
 const loggerServiceURL = process.env.REACT_APP_LOGGER_SVC_URL; // URL of your logger service
 
-export const logClientInfo = (logLevel, message, errorDetails) => {
+const fetchIP = async () => {
+  try {
+    const response = await fetch("https://api.ipify.org?format=json");
+    const data = await response.json();
+    return data.ip;
+  } catch (error) {
+    console.error("Error fetching IP address", error);
+    return window.location.hostname;
+  }
+};
+
+export const logClientInfo = async (logLevel, message, errorDetails) => {
   // Capture client info using UAParser
   const parser = new UAParser();
   const browserInfo = parser.getResult();
-  
-  // Get client IP (this can be enhanced with external APIs to fetch IP)
-  const clientIP = window.location.hostname;
 
   const logData = {
     level: logLevel, // e.g., 'info', 'error'
@@ -17,15 +25,16 @@ export const logClientInfo = (logLevel, message, errorDetails) => {
     isDefault: false,
     label: "Portfolio",
     meta: {
-      browser: browserInfo.browser.name,      
-      os: browserInfo.os.name,                
-      ip: clientIP,
-      errorDetails                 
-    }
+      browser: browserInfo.browser.name,
+      os: browserInfo.os.name,
+      ip: await fetchIP(),
+      errorDetails,
+    },
   };
 
   // Send log data to logger service
-  axios.post(loggerServiceURL, logData)
-    .then(response => console.log('Log sent successfully:', response))
-    .catch(error => console.error('Error sending log:', error));
+  axios
+    .post(loggerServiceURL, logData)
+    .then((response) => console.log("Log sent successfully:", response))
+    .catch((error) => console.error("Error sending log:", error));
 };
